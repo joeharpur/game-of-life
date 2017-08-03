affb4493b458fa8da7939bef5982fc44c97822f8
joeharpur <joeharpur@gmail.com>
2017-08-03 15:54:14 +0100

    Changed grid size and added grid lines. Began implementation og html from options.

--- game_of_life.js
+++ game_of_life.js
@@ -5,28 +5,40 @@
     var width;
     var height;
     var interval_id;
-    var colour = 'green';
+    var form_element;
+    var cell_colour = 'green';
+    var grid_colour = 'gray';
     var curr_gen;
     var next_gen;
     var start =  false;
     var generation = 0;
+    var cell_size;
+    var draw_speed;
 
     document.addEventListener('DOMContentLoaded', init, false);
 
     function init() {    
         canvas = document.querySelector('canvas');
         context = canvas.getContext('2d');
+        form_element = document.querySelector('form');
         width = canvas.width;
         height = canvas.height;
+        cell_size = document.getElementById("cell_size");
+        cell_size = cell_size.options[cell_size.selectedIndex].text;
+        console.log(cell_size);
+        draw_speed = document.getElementById("speed");
+        draw_speed = draw_speed.options[draw_speed.selectedIndex].text;
+        console.log(draw_speed);
         curr_gen = [];
         next_gen = [];
         populate_grid(curr_gen);
         populate_grid(next_gen);
-        window.addEventListener('click', mouse_click, false);
+        canvas.addEventListener('click', mouse_click, false);
         window.addEventListener('keydown', begin, false);
         window.addEventListener('keydown', end, false);
+        form_element.addEventListener('submit', init, false);
 		clearInterval(interval_id);
-        interval_id = window.setInterval(draw, 40);
+        interval_id = window.setInterval(draw, draw_speed);
         
     }
     
@@ -37,12 +49,15 @@
             for (var i=0; i<curr_gen.length; i++) {
                 curr_gen[i] = next_gen[i].slice();
             }
+            generation++;
         }     
-        context.fillStyle = colour;
+        context.fillStyle = cell_colour;
+        context.strokeStyle = grid_colour;
         for (var i=0; i<curr_gen.length; i++) {
             for (var j=0; j<100; j++) {
+                context.strokeRect(i*cell_size, j*cell_size, cell_size, cell_size);
                 if (curr_gen[i][j] === 1) {
-                    context.fillRect(i*5, j*5, 5, 5);
+                    context.fillRect(i*cell_size, j*cell_size, cell_size, cell_size);
                 }
             }
         }
@@ -50,7 +65,7 @@
     } 
 
     function populate_grid(grid) {
-        for (var i=0; i<100; i++) {
+        for (var i=0; i<200; i++) {
             grid.push([]);
             grid[i].push(new Array(100));
 
@@ -61,7 +76,7 @@
     }
 
     function apply_life_rules(curr_gen, next_gen) {
-        for (var i=1; i<99; i++) {
+        for (var i=1; i<199; i++) {
             for (var j=1; j<99; j++) {
                 
                 var neighbours = [curr_gen[i-1][j-1], curr_gen[i-1][j], curr_gen[i-1][j+1],
@@ -89,9 +104,7 @@
                     }
                 }   
             }
-        }
-             
-      
+        }   
     }
 
     function begin(event) {
@@ -113,8 +126,8 @@
         var y = event.y;
         x -= canvas.offsetLeft;
         y -= canvas.offsetTop;
-        x = Math.floor(x/5);
-        y = Math.floor(y/5); 
+        x = Math.floor(x/cell_size);
+        y = Math.floor(y/cell_size); 
         if (curr_gen[x][y]===0) {
             curr_gen[x][y]=1;
             return;
